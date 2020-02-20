3d305da166110be402b5c841a9054395d7823698
Nekiedra Singleton <nekiedrasingleton@gmail.com>
2020-02-19 19:49:26 -0600

    added crud examples

---
+++ database1.sql
@@ -0,0 +1,40 @@
+-- CRUD workout
+
+CREATE DATABASE student;
+
+CREATE TABLE info(id int primary key(id),
+fname VARCHAR(25),
+lname VARCHAR(25),
+phone VARCHAR(15),
+age int,
+email VARCHAR(50) );
+
+insert into info(fname, lname, phone, age, email) VALUES(
+    'Nekiedra', 'Singleton', '6012872671', 21, 'nekeidrasingleton@gmail.com'
+);
+
+ALTER TABLE info MODIFY COLUMN id INT auto_increment;
+
+insert into info(fname, lname, phone, age, email) VALUES(
+    'Random', 'Singleton', '6013625222', 22, 'randomsingleton@gmail.com'
+);
+
+SELECT * from info where id=2;
+
+UPDATE info Set age=55 Where Id=2;
+
+insert into info(fname, lname, phone, age, email) VALUES(
+    'Shareta', 'Harris', '6013625222', 40, 'randomharris@gmail.com'
+);
+
+insert into info(fname, lname, phone, age, email) VALUES(
+    'Andrew', 'Harris', '6013625222', 40, 'randomharris@gmail.com'
+);
+
+CREATE TABLE Birthday(person_id int , Foreign Key(person_id) REFERENCES info(id),
+month VARCHAR(25),
+day int,
+year int
+ );
+
+--  Foreign Key (Customer_SID) REFERENCES CUSTOMER(SID));
